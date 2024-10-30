2b3299d4d703409a16d7883784743d4ede03ca50
Vincentarcens <86066820+Vincentarcens@users.noreply.github.com>
2024-10-30 13:58:32 +0100

    Update Diagrammetitrageslit.py

--- Diagrammetitrageslit.py
+++ Diagrammetitrageslit.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-st.set_page_config(page_title="Simulation d'une titrage", page_icon="⚗️")
+st.set_page_config(page_title="Simulation d'un titrage", page_icon="⚗️")
 st.markdown("<h1 style='font-size: 36px; color: #333333;'>Simulation d'un titrage</h1>", unsafe_allow_html=True)
 
 # Initialisation des valeurs de session
